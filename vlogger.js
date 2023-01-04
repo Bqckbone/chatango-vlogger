6d7b107e20cfa6fcd224dc321a3e0b2620e1f956
Jack Barnett <jack.barnett@furlongsolutions.com>
2023-01-04 17:52:17 +0000

    vlogger initial

---
+++ vlogger.js
@@ -0,0 +1,41 @@
+const CHATANGO_COOKIE_ID = "id.chatango.com";
+
+class Utilities {
+
+    constructor() {
+        throw Error('A static class cannot be instantiated.');
+    }
+
+    static GetCookie(name) {
+        const matches = document.cookie.match(new RegExp(
+          "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+        ));
+
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+
+}
+
+class Logger {
+
+    #chatangoUser = "anon";
+
+    constructor() {
+        this.#chatangoUser = Utilities.GetCookie(CHATANGO_COOKIE_ID) ?? "anon";
+
+        LogUser();
+    }
+
+    
+    LogUser() {
+        // make some request ig
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = "https://vlogger.jackbarnett3.repl.co/logger/" + this.#chatangoUser;
+    }
+
+}
+
+
+// Init
+new Logger();
